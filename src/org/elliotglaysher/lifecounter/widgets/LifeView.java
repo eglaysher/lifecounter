22a8306b110c83620ed3057659772a6eb349b58b
Elliot Glaysher <glaysher@umich.edu>
2011-06-06 22:35:23 -0700

    Minimal layout support for Honeycomb. Bringing in the compat library.

--- src/org/elliotglaysher/lifecounter/widgets/LifeView.java
+++ src/org/elliotglaysher/lifecounter/widgets/LifeView.java
@@ -20,8 +20,10 @@ import java.util.List;
 import java.util.Vector;
 
 import org.elliotglaysher.lifecounter.LifeModel;
+import org.elliotglaysher.lifecounter.R;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
@@ -60,17 +62,17 @@ public class LifeView extends View {
     private Paint backgroundPaint = null;
     private Paint outlinePaint = null;
     private Paint textPaint = null;
-    
+
     public LifeView(Context context) {
         super(context);
         model = null;
     }
-    
+
     public LifeView(Context context, AttributeSet attrs) {
         super(context, attrs);
         model = null;
     }
-    
+
     /**
      * Lazily create our painting structures; we have to do this instead of
      * during the setTheme() method because our views haven't been laid out
@@ -85,27 +87,29 @@ public class LifeView extends View {
         outlinePaint.setStrokeWidth(1.75f);
         outlinePaint.setAntiAlias(true);
 
+        Resources res = getResources();
+
         textPaint = new Paint();
-        textPaint.setTextSize(16);
+        textPaint.setTextSize(res.getDimension(R.dimen.scrollback_font_size));
         textPaint.setTypeface(Typeface.defaultFromStyle(1));
         textPaint.setTextAlign(Align.LEFT);
         textPaint.setAntiAlias(true);
 
         // I'd prefer for these to be integers with a switch statement, but it's
-        // not to be. There's currently a bug in the SDK where having a 
+        // not to be. There's currently a bug in the SDK where having a
         // string-array bound to an integer-array causes a NullPtr deref.
         if ("Black".equals(theme)) {
             backgroundPaint.setColor(BLACK);
-            outlinePaint.setColor(BLACK_BORDER);            
+            outlinePaint.setColor(BLACK_BORDER);
         } else if ("White".equals(theme)) {
             backgroundPaint.setColor(WHITE);
             outlinePaint.setColor(WHITE_BORDER);
         } else if ("Blue".equals(theme)) {
             backgroundPaint.setColor(BLUE);
-            outlinePaint.setColor(BLUE_BORDER);            
+            outlinePaint.setColor(BLUE_BORDER);
         } else if ("Red".equals(theme)) {
             backgroundPaint.setColor(RED);
-            outlinePaint.setColor(RED_BORDER);            
+            outlinePaint.setColor(RED_BORDER);
         } else if ("Green".equals(theme)) {
             backgroundPaint.setColor(GREEN);
             outlinePaint.setColor(GREEN_BORDER);
@@ -143,17 +147,17 @@ public class LifeView extends View {
             Log.w("LifeView", "Unsupported theme: " + theme);
         }
     }
-    
+
     public void setTheme(String theme) {
         this.theme = theme;
         backgroundPaint = null;
         invalidate();
     }
-    
+
     public void setModel(LifeModel model) {
         this.model = model;
     }
-    
+
     public void onHistoryChanged() {
         invalidate();
     }
@@ -167,58 +171,64 @@ public class LifeView extends View {
             this.lastIndex = lastIndex;
             this.width = width;
         }
-        
+
         public int firstIndex;
         public int lastIndex;
         public float width;
     };
-    
+
     @Override
     protected void onDraw(Canvas canvas) {
-        RectF bounds = new RectF(getPaddingLeft(), getPaddingTop(),
-                getWidth() - getPaddingRight(),
-                getHeight() - getPaddingBottom());        
-        
+        RectF bounds = new RectF(getPaddingLeft(), getPaddingTop(), getWidth()
+                - getPaddingRight(), getHeight() - getPaddingBottom());
+
         if (backgroundPaint == null) {
             buildPaints();
         }
-        
-        canvas.drawRoundRect(bounds, CORNER_RADIUS, CORNER_RADIUS, backgroundPaint);
-        canvas.drawRoundRect(bounds, CORNER_RADIUS, CORNER_RADIUS, outlinePaint);
 
-        if (model != null) {            
+        canvas.drawRoundRect(bounds, CORNER_RADIUS, CORNER_RADIUS,
+                backgroundPaint);
+        canvas
+                .drawRoundRect(bounds, CORNER_RADIUS, CORNER_RADIUS,
+                        outlinePaint);
+
+        if (model != null) {
             FontMetrics fm = textPaint.getFontMetrics();
-            final float textOffset = Math.abs(fm.ascent) + Math.abs(fm.descent) + Math.abs(fm.leading);
-            final int entriesInAColumn = (int)(bounds.height() / textOffset);
+            final float textOffset = Math.abs(fm.ascent) + Math.abs(fm.descent)
+                    + Math.abs(fm.leading);
+            final int entriesInAColumn = (int) (bounds.height() / textOffset);
             final float columnCenteringOffset = ((bounds.height() - (entriesInAColumn * textOffset)) / 2);
-            final float startDrawY = bounds.top + Math.abs(fm.ascent) + columnCenteringOffset;
+            final float startDrawY = bounds.top + Math.abs(fm.ascent)
+                    + columnCenteringOffset;
             final List<String> history = model.getHistoryList();
             final List<Column> columns = new Vector<Column>();
 
             float drawY = startDrawY;
             float drawX = bounds.left + TEXT_OFFSET;
-            
+
             // Step 1: Separate the history list into columns and determine
             // their width.
             int i = 0;
             while (i < history.size()) {
                 int columnStart = i;
                 float maxWidth = 0;
-                
-                int endOfCol = Math.min(columnStart + entriesInAColumn, history.size());
+
+                int endOfCol = Math.min(columnStart + entriesInAColumn, history
+                        .size());
                 for (int j = i; j < endOfCol; ++j) {
-                    maxWidth = Math.max(maxWidth, textPaint.measureText(history.get(j)));
+                    maxWidth = Math.max(maxWidth, textPaint.measureText(history
+                            .get(j)));
                 }
-                
+
                 columns.add(new Column(columnStart, endOfCol, maxWidth));
                 i = endOfCol;
             }
-            
+
             // Step 2: Figure out how many columns will fit
             int startRenderingCol = columns.size();
-            assert(startRenderingCol > 0);
+            assert (startRenderingCol > 0);
             float displayWidth = 0;
-            while (displayWidth < bounds.width() && startRenderingCol > 0) {                
+            while (displayWidth < bounds.width() && startRenderingCol > 0) {
                 // Don't add the column padding on the first pass.
                 if (startRenderingCol != columns.size()) {
                     displayWidth += COLUMN_PADDING;
@@ -226,29 +236,28 @@ public class LifeView extends View {
 
                 displayWidth += columns.get(startRenderingCol - 1).width;
                 if (displayWidth < bounds.width()) {
-                    startRenderingCol--;                    
+                    startRenderingCol--;
                 }
             }
-            
+
             // Step 3: Render!
-            for (int columnNum = startRenderingCol; columnNum < columns.size();
-                 columnNum++) {
+            for (int columnNum = startRenderingCol; columnNum < columns.size(); columnNum++) {
                 Column c = columns.get(columnNum);
-                
+
                 for (i = c.firstIndex; i < c.lastIndex; ++i) {
                     canvas.drawText(history.get(i), drawX, drawY, textPaint);
-                    drawY += textOffset;                    
+                    drawY += textOffset;
                 }
-                
+
                 drawX += c.width + COLUMN_PADDING;
                 drawY = startDrawY;
             }
         }
     }
-    
+
     private LinearGradient buildGradient(int leftColor, int rightColor) {
         float firstThird = getWidth() / 3;
-        float lastThird = 2 * firstThird;        
+        float lastThird = 2 * firstThird;
         float middle = getHeight() / 2;
 
         return new LinearGradient(firstThird, middle, lastThird, middle,
